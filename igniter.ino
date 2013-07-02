3b8678b92b593259b432ae2705189cd8b1fa33cb
James Kiefer <zinefer@gmail.com>
2013-07-02 11:36:24 -0600

    Init

---
+++ igniter.ino
@@ -0,0 +1,60 @@
+/*
+
+   )                                                       (
+  /(       __________                     __________       )\
+ {,}______/\         \ Firework Launcher /         /\______{,}
+          \/_________/                   \_________\/
+
+  James Kiefer, 2013
+*/
+
+// pins
+const int CANNONS = 4;
+int buttons[] = {2,3,4,5};
+int relays[]  = {6,7,8,9};
+
+// settings
+const int MODE = 0; //0: instant fire; 1: delayed fire
+const int HOT_TIME = 2; //how long the relay will fire
+int modes[][CANNONS] = {
+   {0,0,0,0},
+   {0,15,30,45},
+};
+
+//vars
+int timeFired[CANNONS];
+int i;
+int overStimulation = 0;
+
+void setup() {
+  //Serial.begin(9600);
+  for (i = 0; i < CANNONS; i++) {
+    pinMode(relays[i], OUTPUT);
+    digitalWrite(buttons[i], HIGH);
+  }
+}
+
+void loop() {
+  int curTime = millis()/1000;
+  for (i = 0; i < CANNONS; i++)
+  {
+    if ( digitalRead(buttons[i]) == LOW ) {
+      if ( timeFired[i] == 0 ) {
+        timeFired[i] = curTime; 
+      } else {
+        overStimulation++;
+      }
+    }
+
+    if (timeFired[i] > 0)
+    {
+     int fireAt = timeFired[i] + modes[MODE][i];
+     if (curTime >= (fireAt+HOT_TIME)) {
+        digitalWrite(relays[i], LOW);
+        timeFired[i] = 0;
+      } else if ( curTime >= fireAt ) {
+        digitalWrite(relays[i], HIGH);
+      }
+    }
+  }
+}
